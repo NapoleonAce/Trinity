167c4dbedfe1c3cb5430939e7b6522ca593a9e22
Unknown <z731630537@163.com>
2019-08-29 11:30:41 +0800

    完善学生管理前端

--- code/src/main/java/edu/zucc/gats/trinity/controller/StudentController.java
+++ code/src/main/java/edu/zucc/gats/trinity/controller/StudentController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/student")
 public class StudentController {
+
     @Autowired
     StudentService studentService;
     @Autowired
@@ -22,6 +23,4 @@ public class StudentController {
     GeneralGradeService generalGradeService;
     @Autowired
     MajorGradeService majorGradeService;
-
-
 }
--- trinity_ui/src/views/stuManView.vue
+++ trinity_ui/src/views/stuManView.vue
@@ -12,7 +12,7 @@
         prefix-icon="el-icon-search">
         <el-select v-model="selectType" slot="prepend" placeholder="请选择" style="width: 120px">
           <el-option label="学生姓名" value="1"></el-option>
-          <el-option label="用户码" value="2"></el-option>
+          <el-option label="学校名" value="2"></el-option>
         </el-select>
         <el-button slot="append" icon="el-icon-search"></el-button>
       </el-input>
@@ -42,6 +42,127 @@
       </el-dialog>
     </el-header>
 
+    <el-main class="my-main">
+      <el-table
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          label="学生ID"
+          width="180">
+          <template slot-scope="scope">
+            <i class="el-icon-tickets"></i>
+            <span style="margin-left: 10px">{{ scope.row.studentId }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="学生姓名"
+          width="180">
+          <template slot-scope="scope">
+            <div slot="reference" class="name-wrapper">
+              {{ scope.row.studentName }}
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="roleName"
+          label="准考证号"
+          width="180"
+         >
+          <template slot-scope="scope">
+            <div slot="reference" class="name-wrapper">
+              {{ scope.row.examId }}
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="性别"
+          width="180"
+          :filters = filterList
+          :filter-method="filterTag"
+          filter-placement="bottom-end">
+          <template slot-scope="scope">
+            <div slot="reference" class="name-wrapper">
+              <el-tag>
+                {{ scope.row.gender }}
+              </el-tag>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-dialog title="编辑信息" :visible.sync="scope.row.dialogFormVisible">
+              <el-form :model="form">
+                <el-form-item label="角色" :label-width="120">
+                  <el-select v-model="form.roleName" placeholder="选择角色">
+                    <el-option label="管理员" value="管理员"></el-option>
+                    <el-option label="学生" value="学生"></el-option>
+                    <el-option label="院校" value="院校"></el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="用户代码" :label-width="120">
+                  <el-input v-model="scope.row.managerCode" auto-complete="off" :disabled="true"></el-input>
+                </el-form-item>
+                <el-form-item label="用户名" :label-width="120">
+                  <el-input v-model="scope.row.managerName" auto-complete="off" :disabled="true"></el-input>
+                </el-form-item>
+                <el-form-item label="新用户名" :label-width="120">
+                  <el-input v-model="form.newManName" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="旧密码" :label-width="120">
+                  <el-input v-model="form.oldPassword" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="新密码" :label-width="120">
+                  <el-input v-model="form.newPassword" auto-complete="off"></el-input>
+                </el-form-item>
+              </el-form>
+              <div slot="footer" class="dialog-footer">
+                <el-button @click="scope.row.dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="handleSave(scope.row)"  >保存</el-button>
+              </div>
+            </el-dialog>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+        <el-table-column label="成绩">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleWatchGeneralGrade(scope.$index, scope.row)">学考</el-button>
+            <el-dialog title={{scope.row.studentName}}+"学考成绩" :visible.sync="scope.row.dialogGeneralGradeVisible">
+              <div slot="footer" class="dialog-footer">
+                <el-button @click="scope.row.dialogGeneralGradeVisible = false">确定</el-button>
+              </div>
+            </el-dialog>
+            <el-button
+              size="mini"
+              @click="handleWatchMajorGrade(scope.$index, scope.row)">选考</el-button>
+            <el-dialog title={{scope.row.studentName}}+"学考成绩" :visible.sync="scope.row.dialogMajorGradeVisible">
+              <div slot="footer" class="dialog-footer">
+                <el-button @click="scope.row.dialogMajorGradeVisible = false">确定</el-button>
+              </div>
+            </el-dialog>
+          </template>
+        </el-table-column>
+        <el-table-column label="特长">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleWatchSpe(scope.$index, scope.row)">查看</el-button>
+            <el-dialog title="编辑信息" :visible.sync="scope.row.dialogSpeVisible">
+              <div slot="footer" class="dialog-footer">
+                <el-button @click="scope.row.dialogSpeVisible = false"> 确定</el-button>
+              </div>
+            </el-dialog>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-main>
 
 
   </el-container>
@@ -51,12 +172,93 @@
   import LeftNav from "@/components/leftNav";
   export default {
     name: "stuManView",
-    components: {LeftNav}
+    components: {LeftNav},
+    data(){
+      return{
+        addDialogVisible:false,
+        dialogSpeVisible:false,
+        form: {
+          manCode:'',
+          oldManName: '',
+          newManName:'',
+          oldPassword:'',
+          newPassword:'',
+          roleName: '',
+          roleId:''
+        },
+        addForm: {
+          manCode:'',
+          manName: '',
+          newPassword:'',
+          roleName: '',
+          roleId:''
+        },
+        filterList:[
+          { text: '男', value: '男' },
+          { text: '女', value: '女' },
+        ],
+        tableData: [
+          {
+            dialogMajorGradeVisible:false,
+            dialogGeneralGradeVisible:false,
+            dialogFormVisible:false,
+            studentId: '1',
+            studentName: '王小虎',
+            examId: '学生',
+            gender:'男',
+            phone:'110552',
+            school:'中二之风中学',
+            province:'天际',
+            generalGrade:[
+              {
+                subjectName:'',
+                grade:''
+              }
+            ],
+            majorGrade:[
+              {
+                subjectName:'',
+                grade:''
+              }
+            ],
+            speciality:[
+              {
+                speType:'',
+                speLevel:'',
+                content:'',
+                evidence:''
+              }
+            ]
+          }
+          ]
+      }
+    },
+    mounted(){
+      this.initStuData();
+    },
+    methods:{
+      initStuData(){
+
+      },
+      handleWatchSpe(index,row){
+
+      },
+      handleWatchGeneralGrade(index,row){
+
+      },
+      handleWatchMajorGrade(index,row){
+
+      },
+    }
   }
 
 </script>
 
 <style scoped>
+  .my-main{
+    margin-left: 300px;
+    margin-top: 2%;
+  }
   .main-container{
     height: 100%;
     padding-left: 0px;
